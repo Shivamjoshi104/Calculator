2db01d558e775bd5c343f2564c4e61ada87ebacb
Shivamjoshi104 <shivam.joshi1283@gmail.com>
2026-02-25 23:06:28 +0530

    Updated calculator logic

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -8,7 +8,10 @@ int main() {
     do {
         std::cout << "\n----- MENU -----\n";
         std::cout << "1. Addition\n";
-        std::cout << "2. Exit\n";
+        std::cout << "2. Subtraction\n";
+        std::cout << "3. Multiplication\n";
+        std::cout << "4. Division\n";
+        std::cout << "5. Exit\n";
         std::cout << "Enter choice: ";
         std::cin >> choice;
 
@@ -18,8 +21,19 @@ int main() {
                 b.input();
                 b.add();
                 break;
-
             case 2:
+                b.input();
+                b.sub();
+                break;
+            case 3:
+                b.input();
+                b.mul();
+                break;
+            case 4:
+                b.input();
+                b.div();
+                break;        
+            case 5:
                 std::cout << "Exiting...\n";
                 break;
 
